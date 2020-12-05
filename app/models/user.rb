e162163d303ab767d6d610169b0bbcc2f03bdbb4
lyonsan <ryoga.yajima@gmail.com>
2020-12-05 15:36:59 +0900

    バリデーション追加、単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'に全角文字（漢字/ひらがな/カタカナ）を使用してください'} do
+         validates :last_name
+         validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'に全角カナを使用してください'} do
+         validates :last_name_kana
+         validates :first_name_kana
+  end
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :email, uniqueness: true
   validates :birthday, presence: true
+  has_many :items
 end
