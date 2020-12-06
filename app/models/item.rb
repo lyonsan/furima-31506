b8f4b98cc8e5bf676b2a76559d7fb16bba20146e
lyonsan <ryoga.yajima@gmail.com>
2020-12-06 22:01:27 +0900

    viewsファイル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,5 +20,5 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :delivery_date_id
   end
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'には半角数字を入力してください'} numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は販売価格の範囲外です' }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'には半角数字を入力してください'}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は販売価格の範囲外です' }
 end
