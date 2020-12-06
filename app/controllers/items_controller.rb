08ab1f569f8690022217f4fa7e44fb5a2133e88c
lyonsan <ryoga.yajima@gmail.com>
2020-12-06 11:37:40 +0900

    ルーティング、アクション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+  def new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
 end
