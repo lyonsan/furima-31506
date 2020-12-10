e5f23b283efd3e7da62455e39db62874b5ee703b
lyonsan <ryoga.yajima@gmail.com>
2020-12-10 23:32:56 +0900

    itemモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,11 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :delivery_date
-  
   with_options presence: true do
     validates :image
     validates :title
     validates :explanation
+    validates :price
   end
   with_options numericality: { other_than: 1 } do
     validates :category_id
@@ -20,5 +20,6 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :delivery_date_id
   end
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'には半角数字を入力してください'}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は販売価格の範囲外です' }
+  validates :price, numericality: { only_integer: true, message: 'には半角数字を入力してください' }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は販売価格の範囲外です' }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image            { Rack::Test::UploadedFile.new(Rails.root.join('app', 'assets', 'images', 'icon_camera.png'), 'image/png') }
     title            { Faker::Lorem.word}
     explanation      { Faker::Lorem.sentence}
     category_id      { Faker::Number.between(from: 2, to: 11) }
@@ -10,5 +9,9 @@ FactoryBot.define do
     delivery_date_id { Faker::Number.between(from: 2, to: 4) }
     price            { Faker::Number.between(from: 300, to: 9999999) }
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/icon_camera.png'), filename: 'icon_camera.png')
+    end
   end
 end
