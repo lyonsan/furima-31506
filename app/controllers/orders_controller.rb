665dfbe8b265aa409c1a6a3e5ff1aaeeff927e3a
lyonsan <ryoga.yajima@gmail.com>
2020-12-15 20:42:17 +0900

    モデル・コントローラ設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,14 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(item_params)
+  end
+
+  def create
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:image, :title, :explanation, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
+end
